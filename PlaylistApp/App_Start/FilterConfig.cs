4410f81e7d3c1981226d1283aec68c4fcaddae72
fariaah13 <fariaah13@gmail.com>
2019-10-03 19:23:07 -0400

    Initial Setup

---
+++ PlaylistApp/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace PlaylistApp
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
