d7cd272eedde38e5fe323473a2e4380ff7091f2e
fariaah13 <fariaah13@gmail.com>
2019-10-03 20:51:38 -0400

    Database and ConnectionStrings

--- Playlist.Data/IdentityModels.cs
+++ Playlist.Data/IdentityModels.cs
@@ -1,5 +1,6 @@
 ﻿using System.Data.Entity;
 using System.Data.Entity.ModelConfiguration;
+using System.Data.Entity.ModelConfiguration.Conventions;
 using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Identity;
@@ -38,7 +39,7 @@ namespace PlaylistApp.Data
 
         protected override void OnModelCreating(DbModelBuilder modelBuilder)
         {
-            odelBuilder
+            modelBuilder
             .Conventions
             .Remove<PluralizingTableNameConvention>();
 
